14406729ec5ee8653f0fc88aa56eecf26fc25b6a
reneyagmur <yagmur.yener@alleninstitute.org>
2026-08-03 18:45:52 +0000

    src init version change

--- src/connects_common_connectivity/__init__.py
+++ src/connects_common_connectivity/__init__.py
@@ -13,7 +13,7 @@ from pydantic import BaseModel
 
 from connects_common_connectivity import models as _models
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 _INTERNAL_CLASSES = frozenset({"ConfiguredBaseModel", "LinkMLMeta"})
 
